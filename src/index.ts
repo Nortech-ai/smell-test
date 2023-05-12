00988c68b88273811b9761ac47800ae49d134375
João Adriano <j.l.d.adriano@gmail.com>
2023-05-12 10:59:17 +0100

    Fix implementation and add quiet mode

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,54 @@
-import { spawn } from "child_process";
+import { ChildProcessByStdio, spawn } from "child_process";
 import { program } from "commander";
+import { Readable } from "stream";
 
 program
-  .version("0.0.1")
+  .version("1.0.0")
   .name("smell-test")
   .description("Runs a cli command and waits on a specific log line to appear")
-  .arguments("<log-line> <command...>")
-  .action(async (logLine: string, command: string[]) => {
-    let didFind = false;
-    const child = spawn(command[0], command.slice(1), {
-      stdio: ["inherit", "pipe", "pipe"],
-    });
-    child.stderr.pipe(process.stderr);
-    child.stdout.pipe(process.stdout);
-    child.stdout.on("data", (data) => {
-      const dataString = data.toString();
-      if (dataString.includes(logLine)) {
-        child.kill("SIGTERM");
-        didFind = true;
-        process.exit(0);
+  .argument("<log-line>", "The log line to wait for")
+  .argument(
+    "<command...>",
+    "The command to run. If you want to pass flags, use --"
+  )
+  .option("--quiet", "Don't output anything")
+  .action(
+    async (
+      logLine: string,
+      command: string[],
+      options: { quiet?: boolean }
+    ) => {
+      const child = spawn(command[0], command.slice(1), {
+        stdio: ["inherit", "pipe", "pipe"],
+      });
+      if (!options.quiet) {
+        child.stderr.pipe(process.stderr);
+        child.stdout.pipe(process.stdout);
       }
-    });
-    child.on("close", (code) => {
-      if (!didFind) {
-        console.error(
-          `[ERROR] Command \`${command.join(
-            " "
-          )}\` did not output log line: ${logLine}`
-        );
+      child.stdout.on("data", (data) => tryFind(data, logLine, child));
+      child.stderr.on("data", (data) => tryFind(data, logLine, child));
+      child.on("close", () => {
+        if (!options.quiet) {
+          console.error(
+            `[ERROR] Command \`${command.join(
+              " "
+            )}\` did not output log line: ${logLine}`
+          );
+        }
         process.exit(1);
-      }
-      process.exit(code ?? 0);
-    });
-  });
+      });
+    }
+  );
 
 program.parse(process.argv);
+function tryFind(
+  data: Buffer,
+  logLine: string,
+  child: ChildProcessByStdio<null, Readable, Readable>
+) {
+  const dataString = data.toString();
+  if (dataString.includes(logLine)) {
+    child.kill("SIGTERM");
+    process.exit(0);
+  }
+}
